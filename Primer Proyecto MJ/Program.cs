09a78019e49502b87641a6e99796554bbda33e19
melanyg7 <melanyjimenez0407@gmail.com>
2024-08-15 13:38:31 -0600

    actualizacion de cambios en el codigo
    recibir y convertir datos

--- Primer Proyecto MJ/Program.cs
+++ Primer Proyecto MJ/Program.cs
@@ -1,3 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+0{
+    Console.WriteLine("Bienvenidos a nuestro primer sistema creado por github");
+
+    Console.WriteLine("Ingrese su nombre para continuar");
+    string Nombre = Console.ReadLine(); //Para recibir valores de caracter
+    Console.WriteLine("Ingrese su edad");
+    int Edad = Convert.ToInt32(Console.ReadLine());//Convertir a dato tipo double
+    double EdadMeses = Edad * 12; //Calculos Matematicos
+    Console.WriteLine(Nombre + "Tu edad en Meses es:" + EdadMeses + "Meses");//concatenacion
+
+}
